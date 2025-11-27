4baa1d1fef0f9de2364942086ed684aa3ec1e944
Manteca-Daniel <danielmanteca.dmg1@gmail.com>
2025-11-27 18:25:27 +0100

    si JAJAJJAJA

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">ESTOY EN MI PRIME</RouterLink>
+        <RouterLink to="/about">JAJAJJAJA</RouterLink>
       </nav>
     </div>
   </header>
